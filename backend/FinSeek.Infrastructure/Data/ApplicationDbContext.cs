c53f87467bcb31e8428e2aac77b60743028604c7
Nazar <politovskyinazar@gmail.com>
2025-11-04 14:38:41 +0200

    DbContext basic confuguration and initial migration added

---
+++ backend/FinSeek.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinSeek.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinSeek.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
