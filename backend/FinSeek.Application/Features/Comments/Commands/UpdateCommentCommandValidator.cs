e07c50f3075a2781c63d9acfcaaf650a92d386a4
Nazar <politovskyinazar@gmail.com>
2026-04-14 17:51:02 +0300

    Add update endpoint for comments

---
+++ backend/FinSeek.Application/Features/Comments/Commands/UpdateCommentCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinSeek.Application.Features.Comments.Commands
+{
+	public class UpdateCommentCommandValidator : AbstractValidator<UpdateCommentCommand>
+	{
+		public UpdateCommentCommandValidator()
+		{
+			RuleFor(x => x.Title)
+				.NotEmpty().WithMessage("Title is required.")
+				.MaximumLength(255).WithMessage("Title must not exceed 255 characters.");
+
+			RuleFor(x => x.Content)
+				.NotEmpty().WithMessage("Content is required.")
+				.MaximumLength(2000).WithMessage("Content must not exceed 2000 characters.");
+		}
+	}
+}
