13bb7f4cf286d4dfce370098771ad5d7ac96c63f
Nazar <politovskyinazar@gmail.com>
2026-04-17 11:54:38 +0300

    Add registration endpoint and seed roles

---
+++ backend/FinSeek.API/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using FinSeek.Application.DTOs.Register;
+using FinSeek.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinSeek.API.Controllers
+{
+	[Route("api/account")]
+	public class AccountController : BaseController
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public AccountController(UserManager<AppUser> userManager)
+        {
+			_userManager = userManager;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+				{
+					return BadRequest(ModelState);
+				}
+
+				var appuser = new AppUser
+				{
+					UserName = registerDto.UserName,
+					Email = registerDto.Email,
+				};
+
+				var createUser = await _userManager.CreateAsync(appuser, registerDto.Password);
+
+				if (createUser.Succeeded)
+				{
+					var roleResult = await _userManager.AddToRoleAsync(appuser, "User");
+
+					if (roleResult.Succeeded)
+					{
+						return Ok("User created");
+					}
+					else
+					{
+						return StatusCode(500, roleResult.Errors);
+					}
+				}
+				else
+				{
+					return StatusCode(500, createUser.Errors);
+				}
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, ex);
+			}
+		}
+    }
+}
---
+++ backend/FinSeek.Application/DTOs/Register/RegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinSeek.Application.DTOs.Register
+{
+	public class RegisterDto
+	{
+		[Required]
+		public string? UserName { get; set; }
+
+		[Required]
+		[EmailAddress]
+		public string? Email { get; set; }
+		
+		[Required]
+		public string? Password { get; set; }
+	}
+}
