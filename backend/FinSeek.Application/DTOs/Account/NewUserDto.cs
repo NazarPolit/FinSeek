2d32c5e372b6d57f39354274ec15ee092df42350
Nazar <politovskyinazar@gmail.com>
2026-04-17 12:33:53 +0300

    Implement Token Service

---
+++ backend/FinSeek.Application/DTOs/Account/NewUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinSeek.Application.DTOs.Account
+{
+	public class NewUserDto
+	{
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public string Token { get; set; }
+	}
+}
--- backend/FinSeek.Application/DTOs/Account/RegisterDto.cs
+++ backend/FinSeek.Application/DTOs/Account/RegisterDto.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinSeek.Application.DTOs.Register
+namespace FinSeek.Application.DTOs.Account
 {
 	public class RegisterDto
 	{
