4ae003bf19165df4df49da067f800e0b26f47bd0
Nazar <politovskyinazar@gmail.com>
2026-05-04 19:00:23 +0300

    Add tailwind styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { CompanySearch } from './company';
 import { searchCompanies } from './api';
 import { ListFormat } from 'typescript';
 import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   const [search, setSearch] = useState<string>("");
@@ -46,6 +48,7 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <Search 
         onSearchSubmit={onSearchSubmit} 
         search = {search} 
