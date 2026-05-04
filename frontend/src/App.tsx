1bb5ec1a174c637201c7337e9e0d4de952f53c0c
Nazar <politovskyinazar@gmail.com>
2026-05-04 16:38:48 +0300

    Refactor to forms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,11 +10,17 @@ function App() {
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string | null>(null);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-      setSearch(e.target.value);
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onPortfolioCreate = (e: SyntheticEvent) => {
+    e.preventDefault();
+    console.log(e);
   }
   
-  const onClick = async (e: SyntheticEvent) => {
+  const onSearchSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
       const result = await searchCompanies(search);
       if(typeof result === "string"){
         setServerError(result);
@@ -26,9 +32,9 @@ function App() {
 
   return (
     <div className="App">
-      <Search onClick={onClick} search = {search} handleChange={handleChange}/>
+      <Search onSearchSubmit={onSearchSubmit} search = {search} handleSearchChange={handleSearchChange}/>
       {serverError && <h1>{serverError}</h1>}
-      <CardList searchResults={searchResult}/>
+      <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate}/>
     </div>
   );
 }
--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import "./Card.css"
 import { JSX } from 'react/jsx-runtime';
 import { CompanySearch } from '../../company';
+import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
 
 interface Props {
   id: string;
   searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void; 
 };
 
-const Card: React.FC<Props> = ({id, searchResult}: Props) : JSX.Element => {
+const Card: React.FC<Props> = ({id, searchResult, onPortfolioCreate}: Props) : JSX.Element => {
   return (
     <div className="card">
       <img
@@ -23,6 +25,7 @@ const Card: React.FC<Props> = ({id, searchResult}: Props) : JSX.Element => {
       <p className="infon">
         {searchResult.exchangeShortName} - {searchResult.stockExchange}
       </p>
+      <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.symbol}/>
     </div>
   );
 };
