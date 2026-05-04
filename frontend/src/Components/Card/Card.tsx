8813cc09d770da48f2ceec543dc99899f6c7ce2f
Nazar <politovskyinazar@gmail.com>
2026-05-04 15:53:08 +0300

    Implement stocks listing with external api

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Card.css"
+import { JSX } from 'react/jsx-runtime';
+import { CompanySearch } from '../../company';
+
+interface Props {
+  id: string;
+  searchResult: CompanySearch;
+};
+
+const Card: React.FC<Props> = ({id, searchResult}: Props) : JSX.Element => {
+  return (
+    <div className="card">
+      <img
+        alt="Company log"
+      />
+      <div className="details">
+        <h2>
+          {searchResult.name} ({searchResult.symbol})
+        </h2>
+        <p>${searchResult.currency}</p>
+      </div>
+      <p className="infon">
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
+      </p>
+    </div>
+  );
+};
+
+export default Card;
