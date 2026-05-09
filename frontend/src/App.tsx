9e505a1b78ea3bea0f54373b8a6bf325e8cf3b2b
NazarPolit <politovskyinazar@gmail.com>
2026-05-09 14:46:51 +0300

    Add AuthService and ErrorHandler for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router';
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import { Outlet } from "react-router";
+import Navbar from "./Components/Navbar/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
---
+++ frontend/src/Helpers/ErrorHandler.tsx
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export const handleError = (error: any) => {
+  if (axios.isAxiosError(error)) {
+    var err = error.response;
+
+    if (Array.isArray(err?.data)) {
+      for (let val of err.data) {
+        toast.warning(val.description);
+      }
+    } 
+    else if (err?.data?.errors && typeof err.data.errors === "object") {
+      for (let e in err.data.errors) {
+        toast.warning(err.data.errors[e][0]);
+      }
+    } 
+    else if (err?.status === 401) {
+      toast.warning(err.data || "Unauthorized. Please login.");
+      window.history.pushState({}, "LoginPage", "/login");
+    } 
+    else if (err?.data) {
+      toast.warning(err.data);
+    }
+  }
+};
