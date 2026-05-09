5dcf6108c2725c821e6a88cdcb53b62b69f44bce
NazarPolit <politovskyinazar@gmail.com>
2026-05-09 14:50:45 +0300

    Add Context for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,15 @@ import Navbar from "./Components/Navbar/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { UserProvider } from "./Context/useAuth";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Navbar />
       <Outlet />
       <ToastContainer />
-    </>
+    </UserProvider>
   );
 }
 
